be445c3136ae44c77796ca0c4614b2d60f17ac15
Moim HA <moimhossain@gmail.com>
2020-03-16 15:49:55 +0100

    Added animations

--- src/extensions/all/Hub/RepositoryTab.tsx
+++ src/extensions/all/Hub/RepositoryTab.tsx
@@ -8,8 +8,10 @@ import { ArrayItemProvider } from "azure-devops-ui/Utilities/Provider";
 import { Icon, IconSize } from "azure-devops-ui/Icon";
 import { Card } from "azure-devops-ui/Card";
 import { ScrollableList, IListItemDetails, ListSelection, ListItem } from "azure-devops-ui/List";
+import { Images } from './../images/Images';
 
 export interface IRepositoryTabState {
+    loading: boolean;
     projectName?: string;
     projectId?: string;
     repos?: any;
@@ -26,7 +28,7 @@ export class RepositoryTab extends React.Component<{}, IRepositoryTabState> {
 
     constructor(props: {}) {
         super(props);
-        this.state = {};
+        this.state = { loading: true };
     }
 
     public componentDidMount() {
@@ -34,28 +36,33 @@ export class RepositoryTab extends React.Component<{}, IRepositoryTabState> {
     }
 
     public render(): JSX.Element {
-        const { repos } = this.state;
+        const { repos, loading } = this.state;
         const mv = new ArrayItemProvider<IRepositoryItem>(repos);
         return (
             <div className="page-content page-content-top flex-column rhythm-vertical-16">
-                {
-                    repos &&
-                    <Card>
-                    <div style={{ display: "flex", height: "300px" }}>
-                        <ScrollableList
-                            itemProvider={mv}
-                            renderRow={this.renderRow}
-                            selection={this.selection}
-                            width="100%"
-                        />
-                    </div>
+                <Card>
+                    {
+                        loading &&
+                        <img style={{ width: 32, height: 32 }} src={(new Images()).getLoadingImage()} />
+                    }
+                    {
+                        (!loading && repos) &&
+                        <div style={{ display: "flex", height: "300px" }}>
+                            <ScrollableList
+                                itemProvider={mv}
+                                renderRow={this.renderRow}
+                                selection={this.selection}
+                                width="100%"
+                            />
+                        </div>
+
+                    }
                 </Card>
-                }
             </div>
         );
     }
 
-    
+
     private renderRow = (
         index: number,
         item: IRepositoryItem,
@@ -65,8 +72,9 @@ export class RepositoryTab extends React.Component<{}, IRepositoryTabState> {
         return (
             <ListItem key={key || "list-item" + index} index={index} details={details}>
                 <div className="list-example-row flex-row h-scroll-hidden">
-                <img alt="" className="contributed-icon contributed-icon-image bolt-image flex-noshrink" 
-                src="https://cdn.vsassets.io/ext/ms.vss-code-web/common-content/Nav-Code.0tJczmQtl3hyKtlh.png" />
+                    <img alt="" className="contributed-icon contributed-icon-image bolt-image flex-noshrink"
+                        style={{ width: 24, height: 24, marginTop: 12 }}
+                        src="https://cdn.vsassets.io/ext/ms.vss-code-web/common-content/Nav-Code.0tJczmQtl3hyKtlh.png" />
                     <div
                         style={{ marginLeft: "10px", padding: "10px 0px" }}
                         className="flex-column h-scroll-hidden"
@@ -79,19 +87,20 @@ export class RepositoryTab extends React.Component<{}, IRepositoryTabState> {
                 </div>
             </ListItem>
         );
-    };    
+    };
 
     private async initialize() {
         const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
         const project = await projectService.getProject();
-        if (project) {       
+        if (project) {
             var repos = await new BackendService(project.id).getAllRepositories();
-            this.setState({ 
+            this.setState({
                 projectName: project.name,
                 projectId: project.id,
-                repos: repos
+                repos: repos,
+                loading: false
             });
             console.log(repos)
-        }        
+        }
     }
 }
